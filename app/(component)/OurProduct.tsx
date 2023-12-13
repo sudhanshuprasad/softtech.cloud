db16b418964dac4c2cc7123fe7cb4a669cb0dbbe
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2023-12-14 00:03:55 +0530

    product catalogue

---
+++ app/(component)/OurProduct.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Button } from 'flowbite-react';
+
+const OurProduct = () => {
+    return (
+        <>
+           {/* <!-- component --> */}
+{/* <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script> */}
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
+<main className="grid min-h-screen w-full place-content-center bg-gray-900">
+    <div x-data="imageSlider" className="relative mx-auto max-w-2xl overflow-hidden rounded-md bg-gray-100 p-2 sm:p-4">
+        <div className="absolute right-5 top-5 z-10 rounded-full bg-gray-600 px-2 text-center text-sm text-white">
+            <span x-text="currentIndex"></span>/<span x-text="images.length"></span>
+        </div>
+
+        <Button  className="absolute left-5 top-1/2 z-10 flex h-11 w-11 -translate-y-1/2 items-center justify-center rounded-full bg-gray-100 shadow-md">
+            <i className="fas fa-chevron-left text-2xl font-bold text-gray-500"></i>
+        </Button>
+
+        {/* <Button @click="forward()" className="absolute right-5 top-1/2 z-10 flex h-11 w-11 -translate-y-1/2 items-center justify-center rounded-full bg-gray-100 shadow-md">
+            <i className="fas fa-chevron-right text-2xl font-bold text-gray-500"></i>
+        </Button> */}
+
+        <div className="relative h-80" style="width: 30rem">
+            <template x-for="(image, index) in images">
+                <div x-show="currentIndex == index + 1" x-transition:enter="transition transform duration-300" x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100" x-transition:leave="transition transform duration-300" x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0" className="absolute top-0">
+                    <img src="image" alt="image" className="rounded-sm" />
+                </div>
+            </template>
+        </div>
+    </div>
+</main>
+
+{/* <script>
+  document.addEventListener("alpine:init", () => {
+    Alpine.data("imageSlider", () => ({
+      currentIndex: 1,
+      images: [
+        "https://unsplash.it/640/425?image=30",
+        "https://unsplash.it/640/425?image=40",
+        "https://unsplash.it/640/425?image=50",
+      ],
+      previous() {
+        if (this.currentIndex > 1) {
+          this.currentIndex = this.currentIndex - 1;
+        }
+      },
+      forward() {
+        if (this.currentIndex < this.images.length) {
+          this.currentIndex = this.currentIndex + 1;
+        }
+      },
+    }));
+  });
+</script> */}
+</>
+    )
+}
+
+export default OurProduct
---
+++ app/(component)/card.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const card = () => {
+    return (
+        <div className='py-10 dark:bg-gray-900 flex content-center justify-center'>
+
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <a href="#">
+                    <img className="rounded-t-lg" src="./S_20231210_142927_0000.svg" alt="" />
+                </a>
+                <div className="p-5">
+                    <a href="#">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Our Product</h5>
+                    </a>
+                    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2023 so far, in reverse chronological order. Save water in a smart way with softtech.cloud</p>
+                    <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                        Buy now
+                        <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg>
+                    </a>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default card
