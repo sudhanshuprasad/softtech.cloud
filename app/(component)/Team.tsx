642fe1f577c76184501b9bc266d935339d5a3152
sudhanshuprasad <sudhanshuprasad.sp@gmail.com>
2023-12-18 14:39:45 +0530

    error fixed team.tsx

--- app/(component)/Team.tsx
+++ app/(component)/Team.tsx
@@ -8,7 +8,7 @@ const Team = () => {
                 <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 rounded-t-lg md:rounded-t-none md:rounded-ss-lg md:border-e dark:bg-gray-800 dark:border-gray-700">
                     <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Very easy this was to integrate</h3>
-                        <p className="my-4">If you care for your time, I hands down would go with this."</p>
+                        <p className="my-4">If you care for your time, I hands down would go with this.&quot;</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center ">
                         <img className="rounded-full w-9 h-9" src="https://img.icons8.com/ios-filled/50/administrator-male--v1.png" alt="profile picture"/>
@@ -21,7 +21,7 @@ const Team = () => {
                 <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
                     <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project</h3>
-                        <p className="my-4">Designing with Figma components that can be easily translated to the utility classNamees of Tailwind CSS is a huge timesaver!"</p>
+                        <p className="my-4">Designing with Figma components that can be easily translated to the utility classNamees of Tailwind CSS is a huge timesaver!&quot;</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center ">
                         <img className="rounded-full w-9 h-9" src="https://img.icons8.com/ios-filled/50/administrator-male--v1.png" alt="profile picture"/>
@@ -34,7 +34,7 @@ const Team = () => {
                 <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-b border-gray-200 md:rounded-es-lg md:border-b-0 md:border-e dark:bg-gray-800 dark:border-gray-700">
                     <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Mindblowing workflow</h3>
-                        <p className="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application."</p>
+                        <p className="my-4">Aesthetically, the well designed components are beautiful and will undoubtedly level up your next application.&quot;</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center ">
                         <img className="rounded-full w-9 h-9" src="https://img.icons8.com/ios-filled/50/administrator-male--v1.png" alt="profile picture"/>
@@ -47,7 +47,7 @@ const Team = () => {
                 <figure className="flex flex-col items-center justify-center p-8 text-center bg-white border-gray-200 rounded-b-lg md:rounded-se-lg dark:bg-gray-800 dark:border-gray-700">
                     <blockquote className="max-w-2xl mx-auto mb-4 text-gray-500 lg:mb-8 dark:text-gray-400">
                         <h3 className="text-lg font-semibold text-gray-900 dark:text-white">Efficient Collaborating</h3>
-                        <p className="my-4">You have many examples that can be used to create a fast prototype for your team."</p>
+                        <p className="my-4">You have many examples that can be used to create a fast prototype for your team&quot;</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center ">
                         <img className="rounded-full w-9 h-9" src="https://img.icons8.com/ios-filled/50/administrator-male--v1.png" alt="profile picture"/>
